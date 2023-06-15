8dc2e65c0cddba30dc018c617fb3f4df1bcf5298
davi5663 <davi5663@edu.sde.dk>
2023-06-15 14:11:02 +0200

    all get request

--- src/index.js
+++ src/index.js
@@ -25,6 +25,97 @@ app.get("/allasset", (req, res) => {
     });
 });
 
+app.get("/asset", (req, res) => {
+  axios({
+    req: "AssetID",
+    url: "allasset/:AssetID",
+    method: "get",
+  })
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      if (!error.response) {
+        // network error
+        this.errorStatus = "Error: Network Error";
+      } else {
+        this.errorStatus = error.response.data.message;
+      }
+    });
+});
+
+app.get("/asset_info", (req, res) => {
+  axios({
+    url: "asset_info",
+    method: "get",
+  })
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      if (!error.response) {
+        // network error
+        this.errorStatus = "Error: Network Error";
+      } else {
+        this.errorStatus = error.response.data.message;
+      }
+    });
+});
+
+app.get("/allcategory", (req, res) => {
+  axios({
+    url: "allcategory",
+    method: "get",
+  })
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      if (!error.response) {
+        // network error
+        this.errorStatus = "Error: Network Error";
+      } else {
+        this.errorStatus = error.response.data.message;
+      }
+    });
+});
+
+app.get("/allbrand", (req, res) => {
+  axios({
+    url: "allbrand",
+    method: "get",
+  })
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      if (!error.response) {
+        // network error
+        this.errorStatus = "Error: Network Error";
+      } else {
+        this.errorStatus = error.response.data.message;
+      }
+    });
+});
+
+app.get("/allcolor", (req, res) => {
+  axios({
+    url: "allcolor",
+    method: "get",
+  })
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      if (!error.response) {
+        // network error
+        this.errorStatus = "Error: Network Error";
+      } else {
+        this.errorStatus = error.response.data.message;
+      }
+    });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
